26ee4ef7f9be2b92893842238d04a4c6af2d46c4
git-chad <tobimocc@gmail.com>
2023-09-13 17:04:03 -0300

    project/[id] page, full responsive, qol small changes

--- src/app/projects/[id]/page.jsx
+++ src/app/projects/[id]/page.jsx
@@ -1,30 +1,55 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import projectsData from "/public/projects.json";
-import Image from "next/image";
+import Lenis from "@studio-freight/lenis";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 const ProjectPage = ({ params }) => {
   const { id } = params;
   console.log(id);
   const project = projectsData.find((proj) => proj.id === id);
 
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   if (!project) {
     return (
-      <div className=" grid-bg w-screen h-screen flex justify-center items-center">
-        <h2 className="text-6xl font-bold">Project not found 😔</h2>
+      <div className="grid-bg w-full h-screen flex justify-center items-center text-center">
+        <h2 className="text-6xl font-bold p-1"><span className="text-red-500">404</span> Project not found</h2>
       </div>
     );
   }
 
   return (
-    <main>
-      <h2>{project.name}</h2>
-      <p>{project.shortDescription}</p>
-      <ul>
-        {project.materials.map((material, index) => (
-          <li key={index}>{material}</li>
-        ))}
-      </ul>
-      <section>photo section</section>
+    <main className="grid-bg w-full h-full">
+      <div className="sm:p-24 overflow-x-hidden mt-32">
+        <h2 className={`${montserrat.className} w-screen text-center sm:text-start text-6xl sm:text-7xl font-black`}>
+          {project.name}
+        </h2>
+        <span className="sub-span"></span>
+        <p className="text-3xl p-2 text-center sm:text-start sm:w-[25ch] my-8">{project.shortDescription}</p>
+        <section className="flex flex-col sm:space-y-36 justify-center items-center mb-24">
+          {project.pictures.map((pic, index) => {
+            return (
+              <img className="min-w-[360px] px-4 pb-4 sm:p-0" key={index} src={pic} />
+            );
+          })}
+        </section>
+      </div>
     </main>
   );
 };
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -4,6 +4,7 @@ import { Montserrat } from "next/font/google";
 import { Cabin } from "next/font/google";
 import Link from "next/link";
 import projectsData from "/public/projects.json";
+import Lenis from "@studio-freight/lenis";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 const cabin = Cabin({
@@ -15,50 +16,51 @@ const cabin = Cabin({
 const Projects = () => {
   const [activeProject, setActiveProject] = useState(null);
   const h2Ref = useRef(null);
+  const spanRef = useRef(null);
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    console.log('effect triggered');
     const h2 = h2Ref.current;
+    const span = spanRef.current;
     setIsMobile(window.innerWidth > 768);
 
     h2.style.opacity = 0;
-    h2.style.transform = "translate(0, 48px)"
-    h2.style.transition = "opacity 0.4s cubic-bezier(0.9, 0.03, 0.69, 0.22), transform 0.4s cubic-bezier(0.9, 0.03, 0.69, 0.22)"
+    h2.style.transform = "translate(0, 48px)";
+    h2.style.transition =
+      "opacity 0.4s cubic-bezier(0.9, 0.03, 0.69, 0.22), transform 0.4s cubic-bezier(0.9, 0.03, 0.69, 0.22)";
 
     setTimeout(() => {
       h2.style.opacity = 1;
-      h2.style.transform = "translate(0, 0px)"
-    }, 300)
+      h2.style.transform = "translate(0, 0px)";
+
+    }, 300);
 
     if (window.innerWidth <= 768) {
       setTimeout(() => {
-        h2.style.opacity = 0;  // You can adjust this as needed
-        h2.style.transform = "translate(0, -48px)";  // Adjust the value as per your design
-      }, 1300);
+        span.style.opacity = 0;
+        h2.style.opacity = 0; // You can adjust this as needed
+        h2.style.transform = "translate(0, -48px)"; // Adjust the value as per your design
+      }, 1000);
     }
-  }, [isMobile])
+  }, [isMobile]);
 
   return (
     <main className="projects grid-bg w-screen h-screen flex flex-col md:flex-row justify-center items-center overflow-auto">
       <div className="h-l md:w-1/3 md:h-full p-8 absolute sm:sticky sm:top-32 top-12">
-        <h2 ref={h2Ref} className={`${montserrat.className} proj-title font-black text-7xl`}>
+        <h2
+          ref={h2Ref}
+          className={`${montserrat.className} proj-title font-black text-7xl`}
+        >
           Projects
         </h2>
-        <div className={`info-section ${activeProject ? "sm:active" : ""}`}>
+        <span ref={spanRef} className="sub-span"></span>
+        <div className={`info-section ${activeProject ? "active" : ""}`}>
           <h3 className="project-title text-3xl my-8 font-bold">
             {activeProject?.name}
           </h3>
           <p className="project-description text-lg mb-4">
             {activeProject?.shortDescription}
           </p>
-          <ul className="project-materials">
-            {activeProject?.materials.map((material, index) => (
-              <li key={index} className="text-sm">
-                {material}
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
 
@@ -72,7 +74,7 @@ const Projects = () => {
           >
             <figure>
               <img
-              className="min-w-[360px] p-4"
+                className="min-w-[360px] p-4"
                 src={project.pictures[0]}
                 alt={project.name}
               />
@@ -80,7 +82,13 @@ const Projects = () => {
                 <figcaption className="italic text-lg">
                   {project.name}
                 </figcaption>
-                <Link href='/posts/[id]' as={`/projects/${project.id}`} className="text-lg font-bold">details</Link>
+                <Link
+                  href="/posts/[id]"
+                  as={`/projects/${project.id}`}
+                  className="text-lg font-bold"
+                >
+                  details
+                </Link>
               </div>
             </figure>
           </section>
